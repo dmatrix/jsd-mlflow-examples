d2ae0b479a9f38daee959e95f1408f6c5fe64c21
Jules Damji <dmatrix@comcast.net>
2018-09-12 15:23:55 -0700

    added some pytorch examples to be possible MLflow experiments

--- keras/setup.py
+++ keras/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='imdbclassifier',
       version='0.5',
-      description='Mlflow modules for binary classification in Keras',
+      description='MLflow modules for binary classification in Keras',
       url='http://github.com/dmatrix/jsd-mflow-examples/keras/imdbclassifier',
       author='Jules S. Damji',
       author_email='jules@databricks.com',
---
+++ pytorch/logistic_regression.py
@@ -0,0 +1,56 @@
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+# X data
+x_data = Variable(torch.Tensor([[1.0], [2.0], [3.0], [4.0]]))
+# Y data with its expected value: labels
+y_data = Variable(torch.Tensor([[0.], [0.], [1.], [1.]]))
+
+
+class Model(torch.nn.Module):
+
+    def __init__(self):
+        """
+        In the constructor we instantiate nn.Linear module
+        """
+        super(Model, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)  # One in and one out
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Variable of input data and we must return
+        a Variable of output data.
+        """
+        y_pred = F.sigmoid(self.linear(x))
+        return y_pred
+
+# our model
+model = Model()
+
+
+# Construct our loss function and an Optimizer. The call to model.parameters()
+# in the SGD constructor will contain the learnable parameters of the two
+# nn.Linear modules which are members of the model.
+criterion = torch.nn.BCELoss(size_average=True)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+# Training loop
+for epoch in range(1000):
+        # Forward pass: Compute predicted y by passing x to the model
+    y_pred = model(x_data)
+
+    # Compute and print loss
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.data[0])
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+# After training
+hour_var = Variable(torch.Tensor([[1.0]]))
+print("predict 1 hour ", 1.0, model(hour_var).data[0][0] > 0.5)
+hour_var = Variable(torch.Tensor([[7.0]]))
+print("predict 7 hours", 7.0, model(hour_var).data[0][0] > 0.5)
